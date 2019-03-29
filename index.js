d395473f000b6345d139db1fa3dc127f8752b1d3
Hemakshi Sachdev <sachdev.hemakshi@gmail.com>
2019-03-29 19:51:17 +0530

    fixed links

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const PORT = process.env.PORT || 8080;
 
 let app = express();
 
-app.use(express.static('bugs'));
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -15,11 +13,11 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/bug/quotedtext', (req, res) => {
+app.get('/bugs/quotedtext', (req, res) => {
     res.sendFile(__dirname + '/bugs/quotedtext.html');
 });
 
-app.get('/html.txt', (req, res) => {
+app.get('/htmlresp', (req, res) => {
     res.send("\"new text\"");
 });
 
@@ -27,11 +25,11 @@ app.get('/plain.txt', (req, res) => {
     res.sendFile(__dirname + '/plain.txt');
 });
 
-app.get('/bug/:id', (req, res) => {
+app.get('/bugs/:id', (req, res) => {
     res.sendFile(__dirname + '/bugs/' + req.params.id + '.html');
 })
 
-app.post('/bug/1284488', (req, res) => {
+app.post('/bugs/1284488', (req, res) => {
     const data = {
         a: ['1', '2', '3'],
         b: [],
